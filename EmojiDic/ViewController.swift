3a6317985ecf189b868d220cf0a02da55e6c7cc3
Tomomi Anekawa <Tomomi@Tomomis-MacBook-Pro.local>
2016-11-01 12:03:16 +0200

    added table view and 2nd screen

--- EmojiDic/ViewController.swift
+++ EmojiDic/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var dacooltableview: UITableView!
+    
+    
+    var emojis = ["😘Καλημέρα","🎾μπάλα","💿シーディー","💗","🐼","🎫"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        dacooltableview.dataSource = self
+        dacooltableview.delegate = self
+        
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+        return cell
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
